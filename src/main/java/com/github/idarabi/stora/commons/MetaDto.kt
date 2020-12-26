e6652b0b2204dae0f3cae31cd92f6a02afcc1c1f
Mohamad Hossein Liaghat <mohamadliaghat@gmail.com>
2020-12-26 17:47:55 +0330

    Refactor MetaDto.

--- src/main/java/com/github/idarabi/stora/commons/MetaDto.kt
+++ src/main/java/com/github/idarabi/stora/commons/MetaDto.kt
@@ -1,11 +1,42 @@
 package com.github.idarabi.stora.commons
 
-import java.io.*
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import java.io.Serializable
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 class MetaDto(val userBucketAndKey: UserIdBucketAndKey,
               val contentType: String? = null,
-              var md5: String? = null,
-              val customMeta: MutableMap<String, String> = mutableMapOf(),
-              val size: Long? = null,
-              val createdDate: Long? = null,
-              val lastAccess: Long = -1) : Serializable
+              val customMeta: MutableMap<String, String> = mutableMapOf()) : Serializable {
+
+    var contentLength: Long = -1
+        private set
+
+    var createdDate: Long = 0
+        private set
+
+    var md5: String = ""
+        private set
+
+    var lastAccess: Long = 0
+        private set
+
+    fun withObjectSize(contentLength: Long): MetaDto {
+        this.contentLength = contentLength
+        return this
+    }
+
+    fun withCreatedDate(createdDate: Long): MetaDto {
+        this.createdDate = createdDate
+        return this
+    }
+
+    fun withMd5(md5: String): MetaDto {
+        this.md5 = md5
+        return this
+    }
+
+    fun withLastAccess(lastAccess: Long): MetaDto {
+        this.lastAccess = lastAccess
+        return this
+    }
+}
